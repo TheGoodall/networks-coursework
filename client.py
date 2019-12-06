21269f863930f829bd4c1fb200c868782cf84cb9
James Goodall <james@frizlette.me>
2019-12-06 01:09:31 +0000

    Large changes to the files

--- client.py
+++ client.py
@@ -2,27 +2,56 @@
 import socket
 import sys
 
+import json
+
 HOST = '127.0.0.1'
 PORT = 12345
-s = None
-for res in socket.getaddrinfo(HOST, PORT, socket.AF_UNSPEC, socket.SOCK_STREAM):
-    af, socktype, proto, canonname, sa = res
+
+# Using arguments (if they exist) to set the host and port
+if len(sys.argv) > 1:
+    HOST = sys.argv[1]
+if len(sys.argv) > 2:
+    PORT = sys.argv[2]
+
+# Creating the socket
+try:
+    s=socket.socket()
+except OSError:
+    print("could not open socket")
+    sys.exit(1)
+
+
+
+
+# Function to make a connection to the server, and send the request passed into it, returning the result
+def make_request(request):
+
     try:
-        s = socket.socket(af, socktype, proto)
-    except OSError as msg:
-        s = None
-        continue
-    try:
-        s.connect(sa)
+        s.connect((HOST,PORT))
     except OSError as msg:
         s.close()
-        s = None
-        continue
-    break
-if s is None:
-    print('could not open socket')
-    sys.exit(1)
-s.sendall(b'Hello, world')
-data = s.recv(1024)
-s.close()
-print('Received', repr(data))
+
+
+    if s is None:
+        print('could not open socket')
+        sys.exit(1)
+
+    s.sendall(json.dumps(request).encode())
+    s.shutdown(socket.SHUT_WR)
+    data = b""
+    while 1:
+        segment = s.recv(1024)
+        if segment:
+            data += segment
+        else:
+            break
+    s.close()
+    return data
+
+
+
+
+
+data = make_request(["GET_BOARDS"])
+
+print(data)
--- server.py
+++ server.py
@@ -1,26 +1,62 @@
-# server program
+# Server program
 import socket
 import sys
 import threading
 
+import json
+
+
+def get_boards():
+    return "get boards"
+
+
+def get_messages(board_title):
+    return "content of message board "+board_title[0]
+def post_message(message_details):
+    return "message posted"
 
 def process_data(data):
+    data = json.loads(data)
 
-    return "hello, world".encode()
+    if len(data) >= 1:
+        if data[0] == "GET_BOARDS":
+            response = get_boards()
+        elif len(data) == 2:
+            if data[0] == "GET_MESSAGES":
+                response = get_messages(data[1])
+            elif data[0] == "POST_MESSAGE":
+                response = post_message(data[1])
+            else:
+                response = "ERROR"
+        else:
+            response = "ERROR"
+    else:
+        response = "ERROR"
+
+    return json.dumps(response).encode()
 
 
 
 
 def handle_connection(conn):
-    data = conn.recv(1024)
+    data = b""
+    while 1:
+
+        segment = conn.recv(1024)
+        if segment:
+            data += segment
+        else:
+            break
     if data:
         data = process_data(data)
-        conn.send(data)
+        conn.sendall(data)
     conn.close()
+
 
 
 HOST = "127.0.0.1"
 PORT = 12345
+
 
 if len(sys.argv) > 1:
     HOST = sys.argv[1]
@@ -29,22 +65,16 @@ if len(sys.argv) > 2:
 
 s = None
 
-for res in socket.getaddrinfo(HOST, PORT, socket.AF_UNSPEC,
-                              socket.SOCK_STREAM, 0, socket.AI_PASSIVE):
-    af, socktype, proto, canonname, sa = res
-    try:
-        s = socket.socket(af, socktype, proto)
-    except OSError as msg:
-        s = None
-        continue
-    try:
-        s.bind(sa)
-        s.listen(1)
-    except OSError as msg:
-        s.close()
-        s = None
-        continue
-    break
+try:
+    s = socket.socket()
+except OSError as msg:
+    s = None
+try:
+    s.bind((HOST,PORT))
+    s.listen(1)
+except OSError as msg:
+    s.close()
+    s = None
 if s is None:
     print('could not open socket')
     sys.exit(1)
